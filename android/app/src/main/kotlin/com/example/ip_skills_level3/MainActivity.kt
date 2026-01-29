0715454a15ab76609e8aa942e988f3f68e9fa368
taqueya <taqueya@example.com>
2026-01-30 00:53:54 +0900

    Fix: Android sleep/wake blackout issue - Disable Impeller and use FlutterFragmentActivity

--- android/app/src/main/kotlin/com/example/ip_skills_level3/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ip_skills_level3/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.ip_skills_level3
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
